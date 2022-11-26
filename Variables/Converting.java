774d3be76e02129f7ae8af2179f73c0fd8b7b285
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-11-25 23:04:45 -0300

    Casting

---
+++ Variables/Converting.java
@@ -0,0 +1,19 @@
+package Variables;
+
+public class Converting{
+
+    public static void main(String[] args){
+
+        float floatingPoint = 3.14f;
+        System.out.println(floatingPoint);
+
+        double wage = 1270.50;
+        int value = (int) wage;
+        System.out.println(value);
+
+        double value1 = 0.2;
+        double value2 = 0.1;
+        double total = value1 + value2;
+        System.out.println(total);
+    }
+}
