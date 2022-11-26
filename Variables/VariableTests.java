d61e74cd9906efa6f1046a669d10473510bcf50c
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-11-25 22:48:57 -0300

    Basics Operations

---
+++ Variables/VariableTests.java
@@ -0,0 +1,20 @@
+package Variables;
+
+public class VariableTests {
+	
+	public static void main (String[] args) {
+		System.out.println("New Test");
+		
+		int age;
+		age = 37;
+		System.out.println(age);
+		
+		age = 30 + 10;
+		System.out.println(age);
+
+		age = 7 * 5 + 2;
+		System.out.println(age);	
+		System.out.println("Age is: "+ age + ", congrats!!");
+
+	}
+}
