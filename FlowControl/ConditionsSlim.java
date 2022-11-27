5faf38e8bfba47f5c50b1b7c62095aad893ad779
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-11-27 13:57:40 -0300

    Decision Making Switch Case (Slim code)

---
+++ FlowControl/ConditionsSlim.java
@@ -0,0 +1,14 @@
+package FlowControl;
+
+public class ConditionsSlim {
+    public static void main(String[] args) {
+        int age = 16;
+        boolean companion = true;
+
+        if (age >= 18 || companion == true) {
+            System.out.println("You can enter the club");
+        } else {
+            System.out.println("You can't enter the club");
+        }
+    }
+}
