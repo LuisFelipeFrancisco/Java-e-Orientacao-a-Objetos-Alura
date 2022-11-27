83e7d82ce86def8fdcf661227c61a5cf1079d995
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-11-27 14:15:52 -0300

    Sum with while

---
+++ FlowControl/Summation.java
@@ -0,0 +1,16 @@
+package FlowControl;
+
+public class Summation {
+
+        public static void main(String[] args) {
+            int count = 0;
+            int sum = 0;
+            while (count <= 10) {
+                sum += count;
+                count++;
+                System.out.println("Sum is: " + sum);
+            }
+            System.out.println(sum);
+        }
+    
+}
