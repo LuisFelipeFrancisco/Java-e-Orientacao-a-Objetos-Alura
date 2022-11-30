b37b7e9a77b48c23a4be97b904ef4aefc678457a
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-11-30 00:13:28 -0300

    Testing

--- ByteBank/TestEmployee.java
+++ ByteBank/TestEmployee.java
@@ -1,11 +1,11 @@
 public class TestEmployee {
-    
+
     public static void main(String[] args) {
         Employee Employee = new Employee();
         Employee.setName("Felipe");
         Employee.setCpf("123.456.789-00");
         Employee.setSalary(2500.0);
-        
+
         System.out.println("Name: " + Employee.getName());
         System.out.println("CPF: " + Employee.getCpf());
         System.out.println("Salary: " + Employee.getSalary());
@@ -15,13 +15,15 @@ public class TestEmployee {
         mananger.setName("Luis");
         mananger.setCpf("987.654.321-00");
         mananger.setSalary(5000.0);
+        mananger.setPassword(1234);
 
+        System.out.println("-------------------------");
+
+        System.out.println(mananger.authenticate(1234));
         System.out.println("Name: " + mananger.getName());
         System.out.println("CPF: " + mananger.getCpf());
         System.out.println("Salary: " + mananger.getSalary());
         System.out.println("Bonus: " + mananger.getBonus());
-        System.out.println("Password: " + mananger.setPassword(1234));
-        System.out.println(mananger.authenticate(1234));
-    
+
     }
 }
