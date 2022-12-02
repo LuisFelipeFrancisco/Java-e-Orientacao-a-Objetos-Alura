08f739b3eeca5e2b9635c71313bd395b2e5c0b4d
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-12-01 21:31:16 -0300

    Inheritance

---
+++ ByteBank/Designer.java
@@ -0,0 +1,13 @@
+
+public class Designer extends Employee {
+
+    public double getBonus() {
+        System.out.println("Designer Bonus");
+        return 200;
+    }
+    
+}
+
+/*
+ * The Designer class inherits the Employee class
+ */
