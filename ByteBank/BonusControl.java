bb8c00ee11e696c968419392d374c6ffc81e3d9b
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-12-01 21:30:17 -0300

    Create BonusControl.java

---
+++ ByteBank/BonusControl.java
@@ -0,0 +1,13 @@
+
+public class BonusControl {
+
+    private double totalBonus = 0;
+    
+    public void register(Employee employee) {
+        this.totalBonus += employee.getBonus();
+    }
+
+    public double getTotalBonus() {
+        return totalBonus;
+    }
+}
