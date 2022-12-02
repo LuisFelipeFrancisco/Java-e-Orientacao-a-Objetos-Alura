2915528de3b7b36c48a612db0222b20dc93cbaef
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-12-01 21:28:34 -0300

    Override

--- ByteBank/CheckingAccount.java
+++ ByteBank/CheckingAccount.java
@@ -18,7 +18,6 @@ public class CheckingAccount extends Account {
 
 }
 
-
 /*
 Constructors are not inherited, so we need to create a constructor for CheckingAccount.
 We can use the super() method to call the constructor of the superclass.
