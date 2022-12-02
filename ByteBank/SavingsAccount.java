5b34bc5070bd6c0687d0922d32b7bd8f327e3f25
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-12-01 21:33:01 -0300

    Override

--- ByteBank/SavingsAccount.java
+++ ByteBank/SavingsAccount.java
@@ -1,6 +1,12 @@
+
 public class SavingsAccount extends Account {
 
     public SavingsAccount(int agency, int number) {
         super(agency, number);
     }
+
+	@Override
+	public void deposit(double value) {
+        this.balance += value;
+	}
 }
