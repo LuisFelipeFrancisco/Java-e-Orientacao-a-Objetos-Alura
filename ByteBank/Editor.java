3466ae3ff6e394dcd7b634ab13871ace9901f119
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-12-01 21:31:40 -0300

    Inheritance

---
+++ ByteBank/Editor.java
@@ -0,0 +1,13 @@
+
+public class Editor extends Employee {
+
+    public double getBonus() {
+        System.out.println("Editor Bonus");
+        return 150;
+    }
+    
+}
+
+/*
+ * The Editor class inherits the Employee class
+ */
