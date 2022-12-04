c57756c4538d7b819e15d1097350a4d58ee712ca
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2022-12-04 00:51:05 -0300

    Custom Exception

--- Java-Stack/src/CustomException.java
+++ Java-Stack/src/CustomException.java
@@ -1,5 +1,5 @@
 
-public class CustomException extends RuntimeException {
+public class CustomException extends Exception {
 
     public CustomException(String msg) {
         super(msg);
--- Java-Stack/src/Flow.java
+++ Java-Stack/src/Flow.java
@@ -12,13 +12,13 @@ public class Flow {
         }
     }
 
-    private static void method1() {
+    private static void method1() throws CustomException {
         System.out.println("Starting Method 1");
         method2();
         System.out.println("Ending Method 1");
     }
 
-    private static void method2() {
+    private static void method2() throws CustomException {
         System.out.println("Starting Method 2");
         throw new CustomException("Something went wrong");
         // System.out.println("Ending Method 2");
@@ -30,4 +30,8 @@ public class Flow {
  * Stack is a data structure that stores data in a LIFO (Last In First Out) manner.
  * Catch blocks are executed in the reverse order of their appearance in the code.
  * Multiple catch can be implemented in a single try block. with a | (pipe) between them.
+ * Custom exceptions must extend RuntimeException or Exception.
+ * If extends RuntimeException, it is an unchecked exception.
+ * If extends Exception, it is a checked exception.
+ * Trows keyword is used to declare an exception.
  */
