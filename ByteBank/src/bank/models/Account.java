e0434cd5b7c1f323ebd021359f9cc6890c295933
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2022-12-07 21:51:30 -0300

    javadoc

--- ByteBank/src/bank/models/Account.java
+++ ByteBank/src/bank/models/Account.java
@@ -1,5 +1,12 @@
 package models;
 
+/**
+ * This class represents an account in a bank.
+ * 
+ * @author LuisFelipeFrancisco
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public abstract class Account {
 
 	protected double balance;
@@ -15,8 +22,19 @@ public abstract class Account {
 		System.out.println("Creating an account...");
 	}
 
+	/**
+	 * This function takes a double value and deposits it into the account.
+	 * 
+	 * @param value The amount of money to deposit.
+	 */
 	public abstract void deposit(double value); // Abstract method
 
+	/**
+	 * If the balance is less than the value, throw an InsufficientBalanceException with the message
+	 * "Insufficient balance".
+	 * 
+	 * @param value The amount to be withdrawn
+	 */
 	public void withdraw(double value) throws InsufficientBalanceException { // Concrete method
 		if (this.balance < value) {
 			throw new InsufficientBalanceException("Insufficient balance");
@@ -24,27 +42,58 @@ public abstract class Account {
 		this.balance -= value;
 	}
 
+	/**
+	 * This function returns the balance of the account
+	 * 
+	 * @return The balance of the account.
+	 */
 	public double getBalance() {
 		return this.balance;
 	}
 
+	/**
+	 * This function transfers a value from this account to the destination account.
+	 * 
+	 * @param value The amount of money to be transferred.
+	 * @param destination The account to which the money will be transferred.
+	 */
 	public void transfer(double value, Account destination) throws InsufficientBalanceException {
 		this.withdraw(value);
 		destination.deposit(value);
 	}
 
+	/**
+	 * This function returns the agency of the account
+	 * 
+	 * @return The agency number.
+	 */
 	public int getAgency() {
 		return this.agency;
 	}
 
+	/**
+	 * This function returns the number of the account
+	 * 
+	 * @return The number of the account.
+	 */
 	public int getNumber() {
 		return this.number;
 	}
 
+	/**
+	 * This function sets the holder of the account.
+	 * 
+	 * @param holder The holder object that will be used to hold the data.
+	 */
 	public void setHolder(Holder holder) {
 		this.holder = holder;
 	}
 
+	/**
+	 * This function returns the value of the static variable total
+	 * 
+	 * @return The total number of accounts.
+	 */
 	public static int getTotal() {
 		return Account.total;
 	}
