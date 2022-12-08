56204a17c97d419064a17c2d45aa58984e1668c3
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2022-12-07 21:28:20 -0300

    javadoc

---
+++ ByteBank/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"models","l":"Account"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"}];updateSearchResults();
--- ByteBank/src/bank/models/Account.java
+++ ByteBank/src/bank/models/Account.java
@@ -1,5 +1,11 @@
 package models;
 
+/**
+ * The Account class represents a bank account.
+ * 
+ * @author LuisFelipeFrancisco
+ * @version 1.0
+*/
 public abstract class Account {
 
 	protected double balance;
@@ -8,6 +14,12 @@ public abstract class Account {
 	public Holder holder;
 	private static int total = 0;
 
+	/**
+	 * The constructor initializes the attributes of the class.
+	 * 
+	 * @param agency The agency number.
+	 * @param number The account number.
+	 */
 	public Account(int agency, int number) { // Constructor
 		Account.total++;
 		this.agency = agency;
@@ -15,8 +27,19 @@ public abstract class Account {
 		System.out.println("Creating an account...");
 	}
 
+	/**
+	 * The deposit function takes a double value and adds it to the balance.
+	 * 
+	 * @param value The amount of money to deposit.
+	 */
 	public abstract void deposit(double value); // Abstract method
 
+	/**
+	 * If the balance is less than the value, throw an InsufficientBalanceException with the message
+	 * "Insufficient balance".
+	 * 
+	 * @param value The amount to be withdrawn
+	 */
 	public void withdraw(double value) throws InsufficientBalanceException { // Concrete method
 		if (this.balance < value) {
 			throw new InsufficientBalanceException("Insufficient balance");
@@ -24,27 +47,58 @@ public abstract class Account {
 		this.balance -= value;
 	}
 
+	/**
+	 * This function returns the balance of the account
+	 * 
+	 * @return The balance of the account.
+	 */
 	public double getBalance() {
 		return this.balance;
 	}
 
+	/**
+	 * This function transfers a value from this account to the destination account.
+	 * 
+	 * @param value The amount of money to be transferred.
+	 * @param destination The account to which the money will be transferred.
+	 */
 	public void transfer(double value, Account destination) throws InsufficientBalanceException {
 		this.withdraw(value);
 		destination.deposit(value);
 	}
 
+	/**
+	 * This function returns the agency of the account
+	 * 
+	 * @return The agency number.
+	 */
 	public int getAgency() {
 		return this.agency;
 	}
 
+	/**
+	 * This function returns the number of the account
+	 * 
+	 * @return The number of the account.
+	 */
 	public int getNumber() {
 		return this.number;
 	}
 
+	/**
+	 * This function sets the holder of the account
+	 * 
+	 * @param holder The holder object that will be used to hold the data.
+	 */
 	public void setHolder(Holder holder) {
 		this.holder = holder;
 	}
 
+	/**
+	 * This function returns the value of the static variable total
+	 * 
+	 * @return The total number of accounts.
+	 */
 	public static int getTotal() {
 		return Account.total;
 	}
