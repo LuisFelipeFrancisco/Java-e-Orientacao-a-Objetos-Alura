c53ccd8932c5b0b72474c1425cca3939f5945419
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2022-12-09 14:24:49 -0300

    java.lang

--- JavaLang/README.md
+++ JavaLang/README.md
@@ -23,4 +23,66 @@ String str = "Hello World";
 str = str.toUpperCase();
 System.out.println(str); // HELLO WORLD
 ```
+### .charAt
 
+```java
+String str = "Hello World";
+char c = str.charAt(0);
+System.out.println(c); // H
+```
+
+### .indexOf
+
+```java
+String str = "Hello World";
+int i = str.indexOf("World");
+System.out.println(i); // 6
+```
+
+### .substring
+
+```java
+String str = "Hello World";
+String sub = str.substring(6);
+System.out.println(sub); // World
+```
+
+### .length
+
+```java
+String str = "Hello World";
+int len = str.length();
+System.out.println(len); // 11
+```
+
+### .concat
+
+```java
+String str = "Hello";
+str = str.concat(" World");
+System.out.println(str); // Hello World
+```
+
+### .isEmpty
+
+```java
+String str = "Hello World";
+boolean empty = str.isEmpty();
+System.out.println(empty); // false
+```
+
+### .trim
+
+```java
+String str = " Hello World ";
+str = str.trim();
+System.out.println(str); // Hello World
+```
+
+### .contains
+
+```java
+String str = "Hello World";
+boolean contains = str.contains("Hello");
+System.out.println(contains); // true
+```
--- JavaLang/src/Test.java
+++ JavaLang/src/Test.java
@@ -3,20 +3,59 @@ public class Test {
     public static void main(String[] args){
 
         String name = "Felype";
-
         System.out.println(name);
 
-        name = name.replace("Felype", "Felipe");
-
+        name = name.replace('y', 'i');
         System.out.println(name);
 
-        name.toLowerCase();
-
+        name = name.toLowerCase();
         System.out.println(name);
 
         name = name.toUpperCase();
-
         System.out.println(name);
+
+        char c = 'I';
+        char d = 'Y';
+
+        name = name.replace(c, d);
+        System.out.println(name);
+
+        char e = name.charAt(3);
+        System.out.println(e);
+
+        int f = name.indexOf('Y');
+        System.out.println(f);
+
+        int g = name.indexOf('Y', 4);
+        System.out.println(g);
+
+        name = name.substring(0, 2);
+        System.out.println(name);
+
+        name = name.concat("lipe");
+
+        System.out.println(name.length());
+
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }
+
+        String empty = "";
+        System.out.println(empty.isEmpty());
+        System.out.println(name.isEmpty());
+
+        String name2 = "    Felipe";
+        System.out.println(name2);
+        System.out.println(name2.trim());
+
+        boolean containsly = name.contains("ly");
+        System.out.println(containsly);
+        boolean containsli = name.contains("li");
+        System.out.println(containsli);
 
     }
 }
+
+/*
+ * Strings are immutable, so when you call a method on a String, it will return a new String.
+ */
