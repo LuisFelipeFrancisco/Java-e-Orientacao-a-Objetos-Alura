b87c57d6198eac231805391c1be3a36257296c65
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2022-12-10 18:12:59 -0300

    type cast

--- ByteBank/src/bank/test/TestAccountsArray.java
+++ ByteBank/src/bank/test/TestAccountsArray.java
@@ -1,5 +1,7 @@
 package test;
+import models.Account;
 import models.CheckingAccount;
+import models.SavingsAccount;
 
 public class TestAccountsArray {
     
@@ -15,6 +17,18 @@ public class TestAccountsArray {
         for (int i = 0; i < accounts.length; i++) {
             System.out.println(accounts[i]);
         }
+
+        Account[] acc = new Account[2];
+
+        acc[0] = new CheckingAccount(333, 1);
+        acc[1] = new SavingsAccount(444, 2);
+        
+        for (int i = 0; i < acc.length; i++) {
+            System.out.println(acc[i]);
+        }
+
+        CheckingAccount refAccount = (CheckingAccount) acc[0];
+        System.out.println(refAccount);
                 
     }
 }
