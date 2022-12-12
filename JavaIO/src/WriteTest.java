6fd55070d01eeee45a29d898bf7bebc888bbc4cc
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2022-12-11 21:55:38 -0300

    feat: Output and Writer

---
+++ JavaIO/src/WriteTest.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class WriteTest {
+    public static void main(String[] args) throws IOException {
+
+        OutputStream fis = new FileOutputStream("name.txt");
+        OutputStreamWriter osw = new OutputStreamWriter(fis);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        bw.write("Luis Felipe Francisco Fermino Ferreia");
+
+        bw.close();
+
+    }
+}
