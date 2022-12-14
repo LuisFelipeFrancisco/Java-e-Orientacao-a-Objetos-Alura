ad8c4e848374f00c87807b4dec39c689386a9451
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2022-12-13 23:46:12 -0300

    feat: .Properties

---
+++ JavaIO/src/PropertiesTest.java
@@ -0,0 +1,29 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Properties;
+
+public class PropertiesTest {
+
+    public static void main(String[] args) throws Exception {
+
+        Properties props = new Properties();
+        props.setProperty("login", "google"); 
+        props.setProperty("password", "123456789321654987");
+        props.setProperty("address", "www.google.com.br");
+
+        props.store(new FileWriter("conf.properties"), "comment"); //must be .properties file
+
+        Properties propsReader = new Properties();
+        propsReader.load(new FileReader("conf.properties"));
+
+        String login = propsReader.getProperty("login");
+        String password = propsReader.getProperty("password");
+        String address = propsReader.getProperty("address");
+
+        System.out.println(login);
+        System.out.println(password);
+        System.out.println(address);
+
+    }
+    
+}
