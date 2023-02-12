8b91fddf395791616cb5140687184c06cc12de5d
Felipe Ferreira <luis.ferreira36@fatec.sp.gov.br>
2023-02-12 15:24:56 -0300

    refactor: Design Pattern Strategy

--- JUnitTDD/src/main/java/br/com/alura/tdd/modelo/PERFORMANCE.java
+++ JUnitTDD/src/main/java/br/com/alura/tdd/modelo/PERFORMANCE.java
@@ -1,6 +1,25 @@
 package br.com.alura.tdd.modelo;
 
+import java.math.BigDecimal;
+
 public enum PERFORMANCE {
-    A_DESEJAR, BOA, EXCELENTE;
-    
+    A_DESEJAR {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.03");
+        }
+    }, BOA {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.15");
+        }
+    }, EXCELENTE {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.20");
+        }
+    };
+
+    public abstract BigDecimal percentualReajuste();
+
 }
--- JUnitTDD/src/main/java/br/com/alura/tdd/service/ReadjustService.java
+++ JUnitTDD/src/main/java/br/com/alura/tdd/service/ReadjustService.java
@@ -8,18 +8,8 @@ import br.com.alura.tdd.modelo.PERFORMANCE;
 public class ReadjustService {
 
     public void readjustment(Funcionario funcionario, PERFORMANCE desempenho) {
-        if (desempenho == PERFORMANCE.A_DESEJAR) {
-            BigDecimal readjustment =  funcionario.getSalario().multiply(new BigDecimal("0.03"));
-            funcionario.readjust(readjustment);
-        }
-        else if (desempenho == PERFORMANCE.BOA) {
-            BigDecimal readjustment =  funcionario.getSalario().multiply(new BigDecimal("0.15"));
-            funcionario.readjust(readjustment);
-        }
-        else if (desempenho == PERFORMANCE.EXCELENTE) {
-            BigDecimal readjustment =  funcionario.getSalario().multiply(new BigDecimal("0.20"));
-            funcionario.readjust(readjustment);
-        }
+        BigDecimal readjustment = funcionario.getSalario().multiply(desempenho.percentualReajuste());
+        funcionario.readjust(readjustment);
     }
     
 }
